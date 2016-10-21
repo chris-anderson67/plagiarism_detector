50bdc0f89de45070650b0f513a9caf958f71223f
chris-anderson67 <chrisja67@gmail.com>
2016-10-21 05:20:48 -0400

    added Tuple abstraction, Ability to read into list of Tuples

--- PlagiarismDetector.py
+++ PlagiarismDetector.py
@@ -3,6 +3,8 @@ import re
 
 # Not using Built-In Python tuples
 # Allows for more flexibility, and our own way of comparing elements
+
+
 class NTuple:
     def __init__(self, strings, size):
         self.size = size
@@ -17,35 +19,49 @@ class NTuple:
     def equal(self, ntuple):
         pass
 
+
+def makeTupleList(wordList, tupleSize):
+    tuples = []
+    for i in range(0, (len(wordList) - tupleSize + 1)):
+        words = wordList[i : (i + tupleSize)]
+        tup = NTuple(words, tupleSize)
+        tuples.append(tup)
+    return tuples
+
+
 def main():
-    if (len(sys.argv) < 4 or len(sys.argv) > 5):
+    if len(sys.argv) < 4 or len(sys.argv) > 5:
         print "Usage: ..."
         sys.exit(1)
 
     inputFile = sys.argv[1]
     refFile = sys.argv[2]
     synFile = sys.argv[3]
-    tupleSize = 3
+    if len(sys.argv) == 5: 
+        tupleSize = int(sys.argv[4])
+    else:
+        tupleSize = 3
 
-    with open(inputFile, 'r') as inputFile: inputs = inputFile.read()
-    with open(refFile, 'r') as inputFile: reference = inputFile.read()
+    with open(inputFile, 'r') as inputFile: inputs = inputFile.read().lower()
+    with open(refFile, 'r') as inputFile: reference = inputFile.read().lower()
     with open(synFile, 'r') as inputFile: synonyms = inputFile.readlines()
 
     inputs = re.findall(r'[^\s!,.?":;0-9]+', inputs)
     reference = re.findall(r'[^\s!,.?":;0-9]+', reference)
 
-    for i, line in enumerate(synonyms): 
-        synonyms[i] = re.findall(r'[^\s!,.?":;0-9]+', line)
+    for i in range(len(synonyms)): 
+        synonyms[i] = synonyms[i].lower()
+        synonyms[i] = re.findall(r'[^\s!,.?":;0-9]+', synonyms[i])
 
-    inputTuples = []
+    inputTuples = makeTupleList(tuple(inputs), tupleSize)
+    refTuples = makeTupleList(tuple(reference), tupleSize)
+    for tupleA in inputTuples:
+        for tupleB in refTuples:
+            print(tupleA.get(), " VS ", tupleB.get())
 
-    print "Tupling:"
-    for i in range(0, (len(inputs) - tupleSize + 1)):
-        strings = inputs[i : (i + tupleSize)]
-        tup = NTuple(strings, tupleSize)
-        inputTuples.append(tup)
-
-    for tup in inputTuples:
-        print tup.get()
+    # for tup in inputTuples:
+    #     print tup.get()
+    # for tup in refTuples:
+    #     print tup.get()
 
 main()
